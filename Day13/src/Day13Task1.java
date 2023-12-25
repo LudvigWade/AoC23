18355cf076b05f084b7e41f6d7fa1d2cfb5fd167
Ludvig Wadenbäck <ludde.ludvig@yahoo.se>
2023-12-25 16:27:34 +0100

    Day13 finished

--- Day13/src/Day13Task1.java
+++ Day13/src/Day13Task1.java
@@ -1,13 +1,105 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Day13Task1 {
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner s = new Scanner(new File("data"));
-		s.useDelimiter(",");
 		int sum = 0;
 		while(s.hasNext()) {
+			ArrayList<String> data = new ArrayList<>();
+			String line = s.nextLine();
+			ArrayList<Integer> indexes = new ArrayList<>();
+			while (line != "") {
+				data.add(line);
+				if (s.hasNextLine()) {
+					line = s.nextLine();
+					if (data.get(data.size()-1).equals(line)) {
+						indexes.add(data.size()-1);
+					}
+				} else {
+					break;
+				}
+			}
+			boolean matchfound = true;
+			int matchind = -1;
+			for (Integer ind : indexes) { //Check if a string is equal to it's mirror
+				matchfound = true;
+				for (int i = 0; i<=ind; i++) {
+					if (ind+1+i > data.size()-1) {
+						break;
+					}
+					if (!data.get(ind-i).equals(data.get(ind+1+i))) {
+						matchfound = false;
+						break;
+					}
+				}
+				if (matchfound) {
+					matchind = ind;
+					break;
+				}
+			}
+			if (matchfound && indexes.size() != 0) {
+				sum += (matchind+1)*100;
+				continue;
+			}
+			indexes.clear();
+			String current = data.get(0);
+			for (int j = 0; j<data.get(0).length()-1; j++) { // Possible mirrors in first string
+				if (current.charAt(j) == current.charAt(j+1)) {
+					StringBuilder sb;
+					if (j < current.length()/2) {
+						sb = new StringBuilder(current.substring(0,j+1));
+						sb.reverse();
+						if (sb.toString().equals(current.substring(j+1, j+1+sb.length()))) {
+							indexes.add(j);
+						}
+					} else {
+						sb = new StringBuilder(current.substring(j+1, current.length()));
+						sb.reverse();
+						if (sb.toString().equals(current.substring(j+1-sb.length(), j+1))) {
+							indexes.add(j);
+						}
+					}
+					
+				}
+			}
+			for (Integer ind : indexes) { // Check if the letters in each string is mirrored around indexes
+				matchfound = true;
+				for (int i = 1; i<data.size(); i++) {
+					current = data.get(i);
+					if (current.charAt(ind) == current.charAt(ind+1)) {
+						StringBuilder sb;
+						if (ind < current.length()/2) {
+							sb = new StringBuilder(current.substring(0,ind+1));
+							sb.reverse();
+							if (!sb.toString().equals(current.substring(ind+1, ind+1+sb.length()))) {
+								matchfound = false;
+								break;
+							}
+						} else {
+							sb = new StringBuilder(current.substring(ind+1, current.length()));
+							sb.reverse();
+							if (!sb.toString().equals(current.substring(ind+1-sb.length(), ind+1))) {
+								matchfound = false;
+								break;
+							}
+						}
+					} else {
+						matchfound = false;
+						break;
+					}
+				}
+				if (matchfound) {
+					matchind = ind;
+					break;
+				}
+			}
+			if (matchfound) {
+				sum += (matchind+1);
+				continue;
+			} else {
+				System.out.println("Error");
+			}
 		}
 		System.out.println(sum);
 	}
--- Day13/src/Day13Task2.java
+++ Day13/src/Day13Task2.java
@@ -1,4 +1,76 @@
+import java.io.*;
+import java.util.*;
 
 public class Day13Task2 {
-
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner s = new Scanner(new File("data"));
+		int sum = 0;
+		while(s.hasNext()) {
+			ArrayList<String> data = new ArrayList<>();
+			String line = s.nextLine();
+			while (line != "") {
+				data.add(line);
+				if (s.hasNextLine()) {
+					line = s.nextLine();
+				} else {
+					break;
+				}
+			}
+			int error;
+			int matchind = -1;
+			for (int k = 0; k<data.size(); k++) { //Check if a string is equal to it's mirror
+				error = 0;
+				for (int i = 0; i<=k; i++) {
+					if (k+1+i > data.size()-1) {
+						break;
+					}
+					for (int j = 0; j<data.get(k).length(); j++) {
+						if (!(data.get(k-i).charAt(j) == data.get(k+1+i).charAt(j))) {
+							error++;
+						}
+					}
+					if (error > 1) {
+						break;
+					}
+				}
+				if (error > 1 || error == 0) {
+					continue;
+				}
+				matchind = k;
+				break;
+			}
+			if (matchind != -1) {
+				sum += (matchind+1)*100;
+				continue;
+			}
+			for (int k = 0; k<data.get(0).length(); k++) { //Check if a string is equal to it's mirror
+				error = 0;
+				for (int i = 0; i<=k; i++) {
+					if (k+1+i > data.get(0).length()-1) {
+						break;
+					}
+					for (int j = 0; j<data.size(); j++) {
+						if (!(data.get(j).charAt(k-i) == data.get(j).charAt(k+1+i))) {
+							error++;
+						}
+					}
+					if (error > 1) {
+						break;
+					}
+				}
+				if (error > 1 || error == 0) {
+					continue;
+				}
+				matchind = k;
+				break;
+			}
+			if (matchind != -1) {
+				sum += (matchind+1);
+			} else {
+				System.out.println("Error");
+			}
+		}
+		System.out.println(sum);
+	}
 }
+
